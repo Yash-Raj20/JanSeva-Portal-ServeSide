99bfbaae5df7b81cdc3cfa67afe9483c9cfd4c1b
Yash-Raj20 <ratneshkumarstm987@gmail.com>
2025-08-01 20:50:47 +0530

    add deploye frontend URL

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const server = http.createServer(app);
 // Middleware
 // -----------------------------
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:8080"],
+  origin: ["http://localhost:3000", "https://jan-seva-portal-client-side.vercel.app/", "http://localhost:8080"],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
@@ -67,7 +67,7 @@ app.use('/api', contactRoutes);
 // -----------------------------
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://jan-seva-portal-client-side.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true
   }
