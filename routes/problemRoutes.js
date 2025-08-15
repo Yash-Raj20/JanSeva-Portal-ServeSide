adb3d42910742c990a31c48c6d4ef63964d27bb9
Yash-Raj20 <ratneshkumarstm987@gmail.com>
2025-08-15 16:30:59 +0530

    fix problem id routes

--- routes/problemRoutes.js
+++ routes/problemRoutes.js
@@ -36,8 +36,16 @@ router.get("/", getAllProblems);
 router.get("/:id/:title", async (req, res) => {
   const { id, title } = req.params;
   try {
-    const problem = await Problem.findById(id).populate("userId", "name profileImage");
-    if (!problem || problem.title !== title) return res.status(404).json({ message: "Problem not found" });
+    const problem = await Problem.findById(id).populate(
+      "userId",
+      "name profileImage"
+    );
+    if (
+      !problem ||
+      problem.title.trim().toLowerCase() !== title.trim().toLowerCase()
+    ) {
+      return res.status(404).json({ message: "Problem not found" });
+    }
     res.json(problem);
   } catch (err) {
     console.error("Error fetching problem by id:", err);
